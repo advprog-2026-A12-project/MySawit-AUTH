4e07d6b7519bbd30b81008bd602850d7c365bf99
susshikan <muhamadh123g@gmail.com>
2026-04-12 20:18:44 +0700

    feat: add reassigment repository

--- src/main/java/id/ac/ui/cs/advprog/auth/repository/BuruhMandorAssignmentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/repository/BuruhMandorAssignmentRepository.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.auth.repository;
 
 import id.ac.ui.cs.advprog.auth.model.BuruhMandorAssignment;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BuruhMandorAssignmentRepository extends JpaRepository<BuruhMandorAssignment, UUID> {
     boolean existsByBuruhIdAndIsActiveTrue(UUID buruhId);
+    Optional<BuruhMandorAssignment> findByBuruhIdAndIsActiveTrue(UUID buruhId);
 }
